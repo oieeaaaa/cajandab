d97b0c195a44e9d78060b7babfb6c4b560d412f3
Joimee Tan Cajandab <joimee.cajandab@codeantheory.com>
2021-04-09 15:54:58 +0800

    ✨ Added textScrable component

---
+++ components/textScramble/textScramble.js
@@ -0,0 +1,79 @@
+import { useCallback, useState, useLayoutEffect, useRef } from "react";
+
+const TextScramble = ({ initialText, texts }) => {
+  const [scrambleCount, setScrambleCount] = useState(0);
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+
+  // vars
+
+  const scrambleChars = "!@#$%^&*()))_+`~" + texts[currentTextIndex];
+  const currentText = texts[currentTextIndex];
+
+  // refs
+
+  const textDisplayRef = useRef(null);
+
+  // callbacks
+
+  const scrambleCharTimeout = useCallback(
+    () =>
+      setTimeout(() => {
+        const { current: textDisplay } = textDisplayRef;
+        const textSegments = currentText.split("");
+        let scrambledText = "";
+
+        for (
+          let textIndex = 0;
+          textIndex < textSegments.length;
+          textIndex += 1
+        ) {
+          const randomChar =
+            scrambleChars[Math.floor(Math.random() * scrambleChars.length)];
+
+          scrambledText += randomChar;
+        }
+
+        textDisplay.textContent = scrambledText;
+
+        setScrambleCount((prevCount) => prevCount + 1);
+      }, 100),
+    [currentTextIndex]
+  );
+
+  const changeTextTimeout = useCallback(() =>
+    setTimeout(() => {
+      setCurrentTextIndex(
+        (prevCurrentTextIndex) => (prevCurrentTextIndex + 1) % texts.length // some math stuff
+      );
+
+      setScrambleCount(0);
+    }, 1000)
+  );
+
+  // effects
+
+  useLayoutEffect(() => {
+    if (!textDisplayRef.current) return;
+
+    const { current: textDisplay } = textDisplayRef;
+
+    if (scrambleCount >= scrambleChars.length) {
+      textDisplay.textContent = texts[currentTextIndex];
+
+      changeTextTimeout();
+
+      return () => {
+        clearTimeout(scrambleCharTimeout);
+      };
+    }
+
+    scrambleCharTimeout();
+
+    return () => clearTimeout(changeTextTimeout);
+  }, [textDisplayRef, scrambleCount]);
+
+  return <span ref={textDisplayRef}>{initialText}</span>;
+};
+
+export default TextScramble;
+
